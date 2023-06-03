42b2c43656f0f8a566b8337e24b4dd7e712d478f
Savitha <gsavitha@protonmail.com>
2023-06-03 11:48:42 +0530

    Added WAB client instead of twilio

--- api/sms.ts
+++ api/sms.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { twiml } from "twilio";
 import { MongoClient, Db } from "mongodb";
+import axios from "axios";
 
 let db: Db;
 
@@ -15,24 +15,44 @@ async function connectDb() {
 }
 connectDb();
 
-const twimlResponse = new twiml.MessagingResponse();
+const WA_API_URL = process.env.WA_API_URL;
+const WA_API_TOKEN = process.env.WA_API_TOKEN;
+
+async function sendMessage(phoneNumber: string, text: string) {
+  await axios.post(
+    `${WA_API_URL}/v1/messages`,
+    {
+      recipient_type: "individual",
+      to: phoneNumber,
+      type: "text",
+      text: {
+        body: text,
+      },
+    },
+    {
+      headers: { Authorization: `Bearer ${WA_API_TOKEN}` },
+    }
+  );
+}
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  const msg = req.body.Body;
+  const msg = req.body.messages[0].body; // this depends on the structure of the webhook event
+  const from = req.body.messages[0].from;
+
   if (msg.startsWith("!add")) {
     const expense = msg.slice(4).trim();
     await db.collection("expenses").insertOne({ expense });
 
-    twimlResponse.message("Expense added!");
-    res.status(200).send(twiml.toString());
+    await sendMessage(from, "Expense added!");
+    res.status(200).send("ok");
   } else if (msg.startsWith("!get")) {
     const items = await db.collection("expenses").find().toArray();
     const expenses = items.map((item) => item.expense).join("\n");
 
-    twimlResponse.message(expenses);
-    res.status(200).send(twiml.toString());
+    await sendMessage(from, expenses);
+    res.status(200).send("ok");
   } else {
-    twimlResponse.message("Unknown Command");
-    res.status(200).send(twiml.toString());
+    await sendMessage(from, "Unknown Command");
+    res.status(200).send("ok");
   }
 };
