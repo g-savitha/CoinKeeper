b075db1381b97d4f454a7f3d4c0eec9aa5cfd69e
Savitha <gsavitha@protonmail.com>
2023-06-03 12:34:28 +0530

    Updated code according to new DB schema

--- api/sms.ts
+++ api/sms.ts
@@ -35,24 +35,91 @@ async function sendMessage(phoneNumber: string, text: string) {
   );
 }
 
+// DB SCHEMA:
+
+/* {
+  _id: ObjectId,
+  command: String, // 'credit' or 'debit' or 'get' or 'limit'
+  amount: Number,
+  category: String,
+  timestamp: Date,
+} */
+
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const msg = req.body.messages[0].body; // this depends on the structure of the webhook event
   const from = req.body.messages[0].from;
 
-  if (msg.startsWith("!add")) {
-    const expense = msg.slice(4).trim();
-    await db.collection("expenses").insertOne({ expense });
-
-    await sendMessage(from, "Expense added!");
-    res.status(200).send("ok");
-  } else if (msg.startsWith("!get")) {
-    const items = await db.collection("expenses").find().toArray();
-    const expenses = items.map((item) => item.expense).join("\n");
-
-    await sendMessage(from, expenses);
-    res.status(200).send("ok");
-  } else {
-    await sendMessage(from, "Unknown Command");
+  const parts = msg.split(" ");
+  if (parts.length < 3) {
+    await sendMessage(
+      from,
+      "Invalid command. Please specify amount and category."
+    );
     res.status(200).send("ok");
   }
+
+  const command = parts[0].toLowerCase();
+
+  let amount: number;
+  try {
+    amount = parseInt(parts[1]);
+  } catch (err) {
+    await sendMessage(from, "Invalid amount. Could not parse a number.");
+    return res.status(200).send("ok");
+  }
+
+  const category = parts[2].slice(2).join(" ");
+
+  if (isNaN(amount) || amount < 0) {
+    await sendMessage(
+      from,
+      "Invalid amount. Please specify a positive number."
+    );
+    res.status(200).send("ok");
+  }
+
+  if (command === "credit" || command === "debit") {
+    try {
+      await db.collection("expenses").insertOne({
+        amount,
+        category,
+        transactionType: command,
+        timestamp: new Date(),
+      });
+    } catch (err) {
+      console.error("Failed to insert transaction into database", err);
+      await sendMessage(
+        from,
+        "Failed to record transaction. Please try again later."
+      );
+      res.status(200).send("ok");
+    }
+
+    await sendMessage(
+      from,
+      `Transaction recorded: ${command} ${amount} ${category}`
+    );
+  } else {
+    await sendMessage(
+      from,
+      "Unknown command. Please start with 'credit' or 'debit'."
+    );
+  }
+
+  // if (msg.startsWith("!add")) {
+  //   const expense = msg.slice(4).trim();
+  //   await db.collection("expenses").insertOne({ expense });
+
+  //   await sendMessage(from, "Expense added!");
+  // //   res.status(200).send("ok");
+  // } else if (msg.startsWith("!get")) {
+  //   const items = await db.collection("expenses").find().toArray();
+  //   const expenses = items.map((item) => item.expense).join("\n");
+
+  //   await sendMessage(from, expenses);
+  //   res.status(200).send("ok");
+  // } else {
+  //   await sendMessage(from, "Unknown Command");
+  //   res.status(200).send("ok");
+  // }
 };
